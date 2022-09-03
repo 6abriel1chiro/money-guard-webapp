07feb4123d1eb6db80a45be428ce9ba8d784a36e
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-09-03 19:35:33 -0400

    Implementadas las trasnacciones, añadibles detalles para una transaccion

--- src/presenter.js
+++ src/presenter.js
@@ -1,32 +1,18 @@
-import multiplicar from "./multiplicador";
-import sumar from "./sumador";
+const monto = document.querySelector("#monto");
+const titulo = document.querySelector("#titulo");
+const detalles = document.querySelector('#detalles')
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-
-
-const button_suma = document.getElementById("suma");
-const button_multi= document.getElementById("multiplicacion");
+const button_registro = document.getElementById("registro");
 
 const div = document.querySelector("#resultado-div");
-const div_mult = document.querySelector("#resultado-mult-div");
 
 
-button_suma.addEventListener("click", (event) => {
+button_registro.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-  div.innerHTML = "<p>" + "resultado suma: " + sumar(firstNumber, secondNumber) + "</p>";
-
-});
-
-button_multi.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div_mult.innerHTML = "<p>" + "resultado multiplicacion: " + multiplicar(firstNumber, secondNumber) + "</p>";
-
+  const valueMonto = Number.parseInt(monto.value);
+  
+  div.innerHTML = "<p>" + "Transacción: " + titulo.value + "</p>" +
+    "<p>" + "Monto: " + valueMonto + "$ </p>" +
+    "<p>" + "detalles: " + detalles.value + "</p>";
 });
