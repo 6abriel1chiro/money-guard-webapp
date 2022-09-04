9a70e5c42fe9eab41e269e57c0f954f28f5259d1
Gabriel Ichiro <gabriel582084@gmail.com>
2022-09-04 16:37:45 -0400

    added first input boxes

---
+++ src/wallet.js
@@ -0,0 +1,13 @@
+var user = 
+	{
+		name: "",
+		transactions: [
+			{
+				name: "Apple iPhone 6, 6g GB",
+				//type: "debit",
+				//category: "Electronics",
+				//date: "12 July, 2016",
+				amount: 1000
+			}
+        ]
+    }
