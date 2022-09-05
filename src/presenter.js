9b980e76aeffcbb723cbbb7ab5985eb28a864ec7
Gabriel Ichiro <gabriel582084@gmail.com>
2022-09-04 20:27:01 -0400

    added register first balance and username finctionality

--- src/presenter.js
+++ src/presenter.js
@@ -1,2 +1,24 @@
-import sumar from "./sumador";
+//import sumar from "./sumador";
+let balance = document.querySelector("#initBalance");
+let username = document.querySelector("#username");
 
+const span = document.querySelector("#total");
+const userDiv = document.querySelector("#user-Div");
+const form = document.querySelector("#first-form");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+   balance = Number.parseFloat(balance.value);
+   username =username.value.toString();
+  console.log(username,balance)
+
+  span.innerHTML =  balance;
+
+  form.style.display="none";
+  userDiv.innerHTML =  "<h2>" + "Welcome: " + username + "</h2>";
+
+
+
+});
